07d0385b4aa887cfc3baec61581a219abf4fe480
Picard <chiyu1468@hotmail.com>
2016-11-07 11:46:29 +0800

    2016/11/07 -- AM

---
+++ src/tw/org/iii/picardtest/ObjectOriented/a01_exe.java
@@ -0,0 +1,20 @@
+package tw.org.iii.picardtest.ObjectOriented;
+
+
+public class a01_exe {
+    public static void main(String[] args){
+        a01_Bike b1 = new a01_Bike();
+
+        System.out.println(b1.getSpeed());
+        for(;b1.getSpeed()<50;)
+            b1.SpeedUp();
+        System.out.println(b1.getSpeed());
+
+        System.out.println("-----------------");
+        a01_Bike b2 = new a01_Bike();
+        long x = 4;
+        byte y = 5;
+//        b2.getSpeed(y);
+
+    }
+}
